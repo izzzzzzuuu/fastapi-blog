9590c27dde8e026918399e457165bc361a8446d8
Izzuddin <izzuddinishak8@gmail.com>
2026-08-27 12:59:16 +0800

    Add Authentication parts, and related templates

--- schemas.py
+++ schemas.py
@@ -8,21 +8,27 @@ class UserBase(BaseModel):
     
 
 class UserCreate(UserBase):
-    pass
+    password: str = Field(min_length=8)
 
-class UserResponse(UserBase):
+class UserPublic(BaseModel):
     model_config = ConfigDict(from_attributes=True)
 
     id: int
+    username: str
     image_file: str | None
     image_path: str
+
+class UserPrivate(UserPublic):
+    email: EmailStr
 
 class UserUpdate(BaseModel):
     username: str | None = Field(default=None, min_length=1, max_length=50)
     email: EmailStr | None = Field(default=None, max_length=120)
     image_file: str | None = Field(default=None, min_length=1, max_length=200)
 
-
+class Token(BaseModel):
+    access_token: str
+    token_type: str
 
 class PostBase(BaseModel):      # DRY concept 
     title: str = Field(min_length=1, max_length=100)
@@ -41,7 +47,7 @@ class PostResponse(PostBase):   # return from api
     id: int
     user_id: int
     date_posted: datetime   # automatically serialize
-    author: UserResponse
+    author: UserPublic
     # ^ the 2 fields generated from systems
 
 
